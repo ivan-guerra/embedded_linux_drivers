0b9705e194f952faa3d1faf8f3c920c5a9b5550a
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-10-03 21:26:49 -0700

    Fix faulty /dev/ path and error check logic.

--- apps/chapter_4/ioctl_test.c
+++ apps/chapter_4/ioctl_test.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	/* First, you need to run "mknod /dev/mydev c 202 0" to create /dev/mydev */
 
-	int my_dev = open("dev/mydev", 0);
-	if (my_dev) {
+	int my_dev = open("/dev/mydev", 0);
+	if (my_dev < 0) {
 		perror("Failed to open device file: /dev/mydev");
 	} else {
 		ioctl(my_dev, 100, 110); /* cmd = 100, arg = 110 */
